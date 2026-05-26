b91dc4f7127884bb8246fe170f83ec21857d1b1e
blackwhale.eth <victor.blackwhale@gmail.com>
2026-05-26 17:33:45 -0500

    fix (hook): gas estimation working

--- src/components/Aggregator/hooks/useOptimismFees/index.ts
+++ src/components/Aggregator/hooks/useOptimismFees/index.ts
@@ -6,18 +6,18 @@ const FEE_ADDRESS = '0x420000000000000000000000000000000000000F';
 
 export const chainsWithOpFees = ['optimism', 'base'];
 
-export const getOptimismFee = async (txData) => {
-	if (typeof window !== 'undefined') return 'Unknown';
+export const getOptimismFee = async (txData: string, chain = 'optimism') => {
+	if (!txData) return 0;
 
-	const provider = providers.optimism;
+	const provider = providers[chain] ?? providers.optimism;
 	const gasContract = new ethers.Contract(FEE_ADDRESS, FEE_ABI, provider);
 
 	try {
 		const gas = await gasContract.getL1Fee(txData);
 
-		return gas / 1e18;
+		return Number(ethers.utils.formatEther(gas));
 	} catch (e) {
 		console.log(e, txData);
-		return 'Unknown';
+		return 0;
 	}
 };
